61586ba10fc1d54a53beefcb58a998e455eb74c9
shrkwy <moviesminister@gmail.com>
2026-09-03 06:08:28 +0530

    chenfeng: fix lineage build script wrong manifest import

--- build/android16-qpr2/build_evox-chenfeng.sh
+++ build/android16-qpr2/build_evox-chenfeng.sh
@@ -12,21 +12,24 @@ rm -rf .repo/local_manifests;
 rm -rf out/soong/.intermediates/system/sepolicy;
 
 
+echo ""
 echo "====================="
 echo "      repo init      "
 echo "====================="
 
 repo init -u https://github.com/Evolution-X/manifest -b bka --depth=1 --git-lfs;
-git clone https://github.com/shrkwy/chenfeng_manifest.git -b main .repo/local_manifests;
+git clone https://github.com/shrkwy/chenfeng_manifest.git -b lineage-23.2 --depth=1 .repo/local_manifests;
 
 
+echo ""
 echo "==================="
 echo "     repo sync     "
 echo "==================="
 
 /opt/crave/resync.sh;
 
-sudo apt-get update && sudo apt-get install patchelf coreutils -y;
+# sudo apt-get update;
+# sudo apt-get install -y patchelf coreutils ccache;
 
 export BUILD_USERNAME=Marcy
 export BUILD_HOSTNAME=foss
@@ -34,6 +37,7 @@ export BUILD_HOSTNAME=foss
 rm -rf build/soong/fsgen;
 
 
+echo ""
 echo "===================="
 echo "   starting build  "
 echo "===================="
@@ -43,17 +47,18 @@ lunch lineage_chenfeng-bp4a-user;
 m evolution -j$(nproc --all);
 
 
+echo ""
 echo "=========================================="
-echo "   uploading to GoFile sharing platform  "
+echo "      uploading to sharing platforms      "
 echo "=========================================="
 
 ZIP=$(find out/target/product/chenfeng -maxdepth 1 -type f -name "*.zip" | head -n 1)
 
 if [ -n "$ZIP" ]; then
     echo "Uploading: $ZIP..."
-    wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
-    chmod +x upload.sh
-    ./upload.sh "$ZIP"
+    wget https://raw.githubusercontent.com/shrkwy/chenfeng/refs/heads/main/tools/upload_util.sh
+    chmod +x upload_util.sh
+    ./upload_util.sh "$ZIP"
 else
     echo "No ROM ZIP found in artifacts!"
     exit 1
